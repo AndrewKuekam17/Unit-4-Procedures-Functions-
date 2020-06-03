6c16821b92f440ebe83f4fe0b36d541a09cdf1db
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-06-03 18:04:05 -0400

    Add files via upload

---
+++ OptionalParametresAndrewK/OptionalParametresAndrewK/OptionalParametersFrom.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace OptionalParametresAndrewK
+{
+    partial class frmOptionalParametersFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFullAddress = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblFullAddress
+            // 
+            this.lblFullAddress.AutoSize = true;
+            this.lblFullAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFullAddress.Location = new System.Drawing.Point(38, 29);
+            this.lblFullAddress.Name = "lblFullAddress";
+            this.lblFullAddress.Size = new System.Drawing.Size(343, 20);
+            this.lblFullAddress.TabIndex = 0;
+            this.lblFullAddress.Text = "Enter the full Address in the fields below: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(27, 109);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(74, 18);
+            this.lblAddress.TabIndex = 2;
+            this.lblAddress.Text = "Address:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 3;
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnit.Location = new System.Drawing.Point(27, 282);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(43, 18);
+            this.lblUnit.TabIndex = 4;
+            this.lblUnit.Text = "Unit:";
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPostalCode.Location = new System.Drawing.Point(27, 226);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(106, 18);
+            this.lblPostalCode.TabIndex = 5;
+            this.lblPostalCode.Text = "Postal Code:";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(27, 166);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(42, 18);
+            this.lblCity.TabIndex = 6;
+            this.lblCity.Text = "City:";
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProvince.Location = new System.Drawing.Point(27, 353);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(79, 18);
+            this.lblProvince.TabIndex = 7;
+            this.lblProvince.Text = "Province:";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.BackColor = System.Drawing.Color.Green;
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(98, 413);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(229, 56);
+            this.btnEnter.TabIndex = 8;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = false;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(107, 107);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(248, 26);
+            this.txtAddress.TabIndex = 9;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCity.Location = new System.Drawing.Point(75, 166);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(165, 26);
+            this.txtCity.TabIndex = 10;
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnit.Location = new System.Drawing.Point(75, 282);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(151, 26);
+            this.txtUnit.TabIndex = 14;
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProvince.Location = new System.Drawing.Point(112, 353);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(149, 26);
+            this.txtProvince.TabIndex = 15;
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPostalCode.Location = new System.Drawing.Point(139, 222);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(188, 26);
+            this.txtPostalCode.TabIndex = 16;
+            // 
+            // frmOptionalParametersFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 495);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.txtUnit);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblUnit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblFullAddress);
+            this.Name = "frmOptionalParametersFrom";
+            this.Text = "Optional Parameters by Andrew K.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFullAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.TextBox txtPostalCode;
+    }
+}
+
---
+++ OptionalParametresAndrewK/OptionalParametresAndrewK/OptionalParametersFrom.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Created by: Andrew Kuekam
+ * Created on: 2020-05-25
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #29 - Optional Paramater
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptionalParametresAndrewK
+{
+    public partial class frmOptionalParametersFrom : Form
+    {
+        public frmOptionalParametersFrom()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintName(String AddressName, String userCity, String userPostalCode, String userProvince)
+        {
+            MessageBox.Show ("Your Address is: " + AddressName + " " + userCity + " " + userPostalCode + " " + userProvince);
+        }
+
+        // Proceduce: PrintName
+        // Input: String fristName, string lastName
+        // Output; void
+        // Description: This produce display a message box with the given two parameter: first and last name.
+
+        public void PrintName(String AddressName, String userPostalCode)
+        {
+            MessageBox.Show (" Your address is: " + AddressName + " " + userPostalCode);
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // Declare local variables
+            String userAddressName, userUnitName, userCity, userPostalCode, userProvince;
+
+            // get names from from taxt boxes
+            userAddressName = this.txtAddress.Text;
+            userUnitName = this.txtUnit.Text;
+            userCity = this.txtCity.Text;
+            userPostalCode = this.txtPostalCode.Text;
+            userProvince = this.txtProvince.Text;
+
+            // make the user a Address Name
+            if (userAddressName == " ")
+            {
+                MessageBox.Show(" Please enter your AddressName. ", " Full Address Program");
+            }
+            // make the user enter a City Name
+            else if (userCity == " ")
+            {
+                MessageBox.Show(" Please enter your City Name. ", " Full City Program");
+            }
+            // make the user enter a Postal Name
+            else if (userPostalCode == " ")
+            {
+                MessageBox.Show(" Please enter your Postal Code Name. ", " Full Postal Code Program");
+            }
+            // make the user enter a Postal Name
+            else if (userProvince == " ")
+            {
+                MessageBox.Show(" Please enter your Province Name. ", " Full Province Code Program");
+            }
+            // if the does not enter a UnitName, call the proceduce that accepts only 2 String parameters
+            else if (userUnitName == " ")
+            {
+                PrintName(userAddressName, userCity, userPostalCode, userProvince);
+            }
+            // if the user enter all three names, call the  proceduce that accepts all 3  String parameters
+            else
+            {
+                PrintName(userAddressName, userPostalCode, userCity, userProvince);
+
+            }
+        }
+    }
+}
