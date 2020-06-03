a2cbece5b255073b3a7b63b24ad52b81110c8c35
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-06-03 18:12:42 -0400

    Add files via upload

---
+++ SwitchStateAndrew/SwitchStateAndrew/Switch StatememtForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SwitchStateAndrew
+{
+    partial class frmSwitchStatement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade.Location = new System.Drawing.Point(56, 37);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(165, 20);
+            this.lblGrade.TabIndex = 0;
+            this.lblGrade.Text = "Enter a grade level:";
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(227, 37);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(100, 20);
+            this.txtGrade.TabIndex = 1;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(125, 69);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(150, 53);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = false;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // frmSwitchStatement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(436, 134);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.lblGrade);
+            this.Name = "frmSwitchStatement";
+            this.Text = "Switch Statement By Andrew k.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ SwitchStateAndrew/SwitchStateAndrew/Switch StatememtForm.cs
@@ -0,0 +1,133 @@
+﻿/*
+ * Created by: Andrew Kuekam
+ * Created on: 2020-05-27
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #32 - Percentage Program
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchStateAndrew
+{
+    public partial class frmSwitchStatement : Form
+    {
+        public frmSwitchStatement()
+        {
+            InitializeComponent();
+        }
+
+        //Function: ConvertToPercent
+        // Input: Sring level
+        // OutPut:int
+        private int ConvertToPercent(String Level)
+        {
+            int percentage = -1;
+
+            switch (Level)
+            {
+                case "4++":
+                    percentage = 98;
+                    break;
+                case "4+":
+                    percentage = 95;
+                    break;
+                case "4":
+                    percentage = 85;
+                    break;
+                case "4-":
+                    percentage = 80;
+                    break;
+                case "3+":
+                    percentage = 78;
+                    break;
+                case "3":
+                    percentage = 75;
+                    break;
+                case "3-":
+                    percentage = 72;
+                    break;
+                case "2+":
+                    percentage = 68;
+                    break;
+                case "2":
+                    percentage = 65;
+                    break;
+                case "2-":
+                    percentage = 62;
+                    break;
+                case "1+":
+                    percentage = 58;
+                    break;
+                case "1":
+                    percentage = 55;
+                    break;
+                case "1-":
+                    percentage = 52;
+                    break;
+                case "0+":
+                    percentage = 45;
+                    break;
+                case "0":
+                    percentage = 40;
+                    break;
+                case "0-":
+                    percentage = 35;
+                    break;
+                case "0--":
+                    percentage = 30;
+                    break;
+                case "0---":
+                    percentage = 20;
+                    break;
+                case "0----":
+                    percentage = 10;
+                    break;
+                default:
+                    percentage = -1;
+                    break;
+            }
+            return percentage;
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // declare variables
+            String level;
+            int percent;
+
+            // get level from to text
+            level = this.txtGrade.Text;
+
+            // call the function that will convert the level to a percent
+            percent = ConvertToPercent(level);
+
+            if (percent == +4)
+            {
+                MessageBox.Show(" Please enter a valid level.");
+            }
+            else if (percent == +3)
+            {
+                MessageBox.Show(" Please enter a valid level.");
+            }  
+            else if (percent == -1)
+            {
+                MessageBox.Show(" Please enter a valid level.");
+            }
+            else if (percent == +2)
+            {
+                MessageBox.Show(" Please enter a valid level.");
+            }
+            // Display the percentage to the user
+            MessageBox.Show(" level " + level + " is equivalent to " + percent + " %. ");
+
+        }
+    }
+}
