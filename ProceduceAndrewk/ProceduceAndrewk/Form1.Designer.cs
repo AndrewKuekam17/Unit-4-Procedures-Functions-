3d1299b0c5a02dd3a39627d0812805e5b66c0e58
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-06-03 18:08:04 -0400

    Add files via upload

---
+++ ProceduceAndrewk/ProceduceAndrewk/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ProceduceAndrewk
+{
+    partial class frmAreaProcedure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRectangle = new System.Windows.Forms.Label();
+            this.btnCalculator = new System.Windows.Forms.Button();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblRectangle
+            // 
+            this.lblRectangle.AutoSize = true;
+            this.lblRectangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRectangle.Location = new System.Drawing.Point(57, 156);
+            this.lblRectangle.Name = "lblRectangle";
+            this.lblRectangle.Size = new System.Drawing.Size(344, 20);
+            this.lblRectangle.TabIndex = 0;
+            this.lblRectangle.Text = "Entre the tamperature in degrees Celsius:";
+            // 
+            // btnCalculator
+            // 
+            this.btnCalculator.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCalculator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculator.Location = new System.Drawing.Point(121, 222);
+            this.btnCalculator.Name = "btnCalculator";
+            this.btnCalculator.Size = new System.Drawing.Size(241, 72);
+            this.btnCalculator.TabIndex = 5;
+            this.btnCalculator.Text = "Convert to fahrenheit";
+            this.btnCalculator.UseVisualStyleBackColor = false;
+            this.btnCalculator.Click += new System.EventHandler(this.btnCalculator_Click);
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(407, 158);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 6;
+            // 
+            // frmAreaProcedure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(585, 450);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.btnCalculator);
+            this.Controls.Add(this.lblRectangle);
+            this.Name = "frmAreaProcedure";
+            this.Text = "Area Procedure By Andrew K.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRectangle;
+        private System.Windows.Forms.Button btnCalculator;
+        private System.Windows.Forms.TextBox txtCelsius;
+    }
+}
+
---
+++ ProceduceAndrewk/ProceduceAndrewk/Form1.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by: Andrew kuekam
+ * Created on: 2020-05-22
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Area Procedure
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProceduceAndrewk
+{
+    public partial class frmAreaProcedure : Form
+    {
+        public frmAreaProcedure()
+        {
+            InitializeComponent();
+        }
+        // Function: Degrees celsius
+        // Input: UserCelsius
+        // Output:void 
+        public void Calculatefahrenheit(int Celsius)
+        {
+            // declare local varibles
+            int fahrenheit;
+
+            // Calculate the The Degrees celsius.
+            fahrenheit = Celsius * (int) 9/ (int) 5 + 32;
+
+            // Display a message box with the calculate fahrenheit
+            MessageBox.Show(" The Degrees celsius: " + fahrenheit + " Degrees fahrenheit.");
+        }
+
+        private void btnCalculator_Click(object sender, EventArgs e)
+        {
+            //Declare local variable
+            int UserCelsius;
+
+            // convert the celsius to int 
+            UserCelsius = Convert.ToInt16(this.txtCelsius.Text);
+
+            // call the function to calculate celsius
+            this.Calculatefahrenheit(UserCelsius);
+
+        }
+    }
+}
